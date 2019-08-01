72e779e8e036b8b24c745e58e8ec237f155f6ef2
Mukesh <tsmukesh99@gmail.com>
2019-08-01 12:55:03 +0530

    Everything has been made dynamic

--- index.js
+++ index.js
@@ -1,7 +1,261 @@
 $('#fromdatepicker').datepicker();
 $('#todatepicker').datepicker();
 
+var toDate, fromDate;
+
+var rating = {
+    "gplay" :{
+      "count" : 23,
+      "average" : 2.34
+    },
+    "app" :{
+      "count" : 23,
+      "average" : 2.34
+    }
+}; 
+
+var charts ={
+    "app": {
+        "avg" : [3.721, 3.775, 3.806, 3.829, 3.837,4.0],
+        "col1" : {
+            "jan1" : 1053,
+            "jan2" : 1594,
+            "jan3" : 1146,
+            "jan4" : 977,
+            "jan5" : 341,
+            "jan6" : 250
+          },
+          "col2" : {
+            "jan1" : 323,
+            "jan2" : 498,
+            "jan3" : 279,
+            "jan4" : 244,
+            "jan5" : 356,
+            "jan6" : 250
+          },
+          "col3" : {
+            "jan1" : 241,
+            "jan2" : 252,
+            "jan3" : 153,
+            "jan4" : 153,
+            "jan5" : 153,
+            "jan6" : 250
+          },
+          "col4" : {
+            "jan1" : 152,
+            "jan2" : 109,
+            "jan3" : 106,
+            "jan4" : 82,
+            "jan5" : 82,
+            "jan6" : 250
+          },
+          "col5" : {
+            "jan1" : 462,
+            "jan2" : 294,
+            "jan3" : 295,
+            "jan4" : 274,
+            "jan5" : 955,
+            "jan6" : 250
+          }
+    },
+    "gplay" : {
+        "avg" : [3.721, 3.775, 3.806, 3.829, 3.837,4.0],
+        "col1" : {
+          "jan1" : 1053,
+          "jan2" : 1594,
+          "jan3" : 1146,
+          "jan4" : 977,
+          "jan5" : 341,
+          "jan6" : 250
+        },
+        "col2" : {
+          "jan1" : 323,
+          "jan2" : 498,
+          "jan3" : 279,
+          "jan4" : 244,
+          "jan5" : 356,
+          "jan6" : 250
+        },
+        "col3" : {
+          "jan1" : 241,
+          "jan2" : 252,
+          "jan3" : 153,
+          "jan4" : 153,
+          "jan5" : 153,
+          "jan6" : 250
+        },
+        "col4" : {
+          "jan1" : 152,
+          "jan2" : 109,
+          "jan3" : 106,
+          "jan4" : 82,
+          "jan5" : 82,
+          "jan6" : 250
+        },
+        "col5" : {
+          "jan1" : 462,
+          "jan2" : 294,
+          "jan3" : 295,
+          "jan4" : 274,
+          "jan5" : 955,
+          "jan6" : 250
+        }
+    }
+};
+
+var tables = {
+    "gplay" :{
+      "row1" : {
+        "rated 1-3": "1",
+        "count" : "2",
+        "proportions(%)" : "3"
+      },
+      "row2" : {
+        "rated 1-3": "1",
+        "count" : "2",
+        "proportions(%)" : "3"
+      },
+      "row3" : {
+        "rated 1-3": "1",
+        "count" : "2",
+        "proportions(%)" : "3"
+      },
+      "row4" : {
+        "rated 1-3": "1",
+        "count" : "2",
+        "proportions(%)" : "3"
+      },
+      "row5" : {
+        "rated 1-3": "1",
+        "count" : "2",
+        "proportions(%)" : "3"
+      },
+      "row6" : {
+        "rated 1-3": "1",
+        "count" : "2",
+        "proportions(%)" : "3"
+      },
+      "row7" : {
+        "rated 1-3": "1",
+        "count" : "2",
+        "proportions(%)" : "3"
+      },
+      "row8" : {
+        "rated 1-3": "1",
+        "count" : "2",
+        "proportions(%)" : "3"
+      },
+      "row9" : {
+        "rated 1-3": 1,
+        "count" : 2,
+        "proportions(%)" : 3
+      },
+      "row10" : {
+        "rated 1-3": " ",
+        "count" : " ",
+        "proportions(%)" :  " "      
+      }
+    },
+    "app" :{
+        "row1" : {
+            "rated 1-3": "1",
+            "count" : "2",
+            "proportions(%)" : "3"
+          },
+          "row2" : {
+            "rated 1-3": "1",
+            "count" : "2",
+            "proportions(%)" : "3"
+          },
+          "row3" : {
+            "rated 1-3": "1",
+            "count" : "2",
+            "proportions(%)" : "3"
+          },
+          "row4" : {
+            "rated 1-3": "1",
+            "count" : "2",
+            "proportions(%)" : "3"
+          },
+          "row5" : {
+            "rated 1-3": "1",
+            "count" : "2",
+            "proportions(%)" : "3"
+          },
+          "row6" : {
+            "rated 1-3": "1",
+            "count" : "2",
+            "proportions(%)" : "3"
+          },
+          "row7" : {
+            "rated 1-3": "1",
+            "count" : "2",
+            "proportions(%)" : "3"
+          },
+          "row8" : {
+            "rated 1-3": "1",
+            "count" : "2",
+            "proportions(%)" : "3"
+          },
+          "row9" : {
+            "rated 1-3": 1,
+            "count" : 2,
+            "proportions(%)" : 3
+          },
+          "row10" : {
+            "rated 1-3": 1,
+            "count" : 2,
+            "proportions(%)" : 3      
+          }
+    }
+};
+
+//To set ratings
+function setRating(){
+    document.getElementById("gplay-rating").innerHTML = rating.gplay.count;
+    document.getElementById("app-rating").innerHTML = rating.app.count; 
+    document.getElementById("gplay-average").innerHTML = rating.gplay.average;
+    document.getElementById("app-average").innerHTML = rating.app.average; 
+}
+
+//To create table for app
+function createTableApp(){
+    for(var i = 0; i<10; i++){
+        var key = Object.keys(tables.app);
+        var val = Object.values(tables.app[key[i]]);
+        //Inserting a row
+        var table = document.getElementById("myTable-app");
+        var row = table.insertRow(i+1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = val[0] != " " ? val[0] : "-";
+        cell2.innerHTML = val[1] != " " ? val[1] : "-";
+        cell3.innerHTML = val[2] != " " ? val[2] : "-";
+    }
+}
+
+//To create table for gplay
+function createTableGplay(){
+    for(var i = 0; i<10; i++){
+        var key = Object.keys(tables.gplay);
+        var val = Object.values(tables.gplay[key[i]]);
+        //Inserting a row
+        var table = document.getElementById("myTable-gplay");
+        var row = table.insertRow(i+1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        cell1.innerHTML = val[0] != " " ? val[0] : "-";
+        cell2.innerHTML = val[1] != " " ? val[1] : "-";
+        cell3.innerHTML = val[2] != " " ? val[2] : "-";
+    }
+}
+
+//Chart of google play
 function chart1(){
+    var keys;
+    keys = Object.keys(charts.app.col1);
     chart = new Highcharts.Chart({
         chart: {
           renderTo: 'container',
@@ -31,11 +285,7 @@ function chart1(){
           href: 'https://www.getcloudcherry.com/'
         },
         xAxis: [{
-          categories: ['Jan 2017', 'Feb 2017', 'Mar 2017', 'May 2017',
-            'Apr 2017',
-            'Jun 2017', 'Jul 2017', 'Aug 2017', 'Sep 2017', 'Oct 2017', 'Nov 2017', 'Dec 2017', 'Jan 2018',
-            'Feb 2018', 'Mar 2018', 'Apr 2018', 'May 2018', 'June 2018'
-          ],
+          categories: keys,
         }],
         yAxis: [{ // Primary yAxis
           labels: {
@@ -72,7 +322,7 @@ function chart1(){
             name: 'Cumulative average',
             type: 'line',
     
-            data: [3.721, 3.775, 3.806, 3.829, 3.837],
+            data: charts.gplay.avg,
             color: '#888888',
             zIndex: 2
     
@@ -81,15 +331,14 @@ function chart1(){
             name: '5',
             type: 'column',
             yAxis: 1,
-            data: [1053, 1594, 1146, 977, 341
-            ],
+            data: Object.values(charts.gplay.col1),
             color: '#0E9D58'
     
           }, {
             name: '4',
             type: 'column',
             yAxis: 1,
-            data: [323, 498, 279, 244, 356],
+            data: Object.values(charts.gplay.col2),
             color: '#BFD047'
     
           },
@@ -97,157 +346,165 @@ function chart1(){
             name: '3',
             type: 'column',
             yAxis: 1,
-            data: [241, 252, 153, 153,153],
+            data: Object.values(charts.gplay.col3),
             color: '#FFC105'
     
           }, {
             name: '2',
             type: 'column',
             yAxis: 1,
-            data: [152, 109, 106, 82,82],
+            data: Object.values(charts.gplay.col4),
             color: '#EF7E14'
     
           }, {
             name: '1',
             type: 'column',
             yAxis: 1,
-            data: [462, 294, 295, 274, 955],
+            data: Object.values(charts.gplay.col5),
             color: '#D36259'
     
           }
         ]
       });
-  }
+}
   
-  function chart2(){
-      chart = new Highcharts.Chart({
-        chart: {
-          renderTo: 'container1',
-          zoomType: 'xy'
+//Chart of app
+function chart2(){
+    var keys;
+    keys = Object.keys(charts.gplay.col1);
+    chart = new Highcharts.Chart({
+    chart: {
+        renderTo: 'container1',
+        zoomType: 'xy'
+    },
+    title: {
+        text: ''
+    },
+    tooltip: {
+        formatter: function () {
+        return 'The value for <b>' + this.x + '</b> is <b>' + this.y /* + '</b>, in series ' + this.series.name;*/
+        }
+    },
+    plotOptions: {
+        column: {
+        stacking: 'normal'
         },
+        line: {
+        dataLabels: {
+            enabled: true
+        },
+        enableMouseTracking: false
+        }
+    },
+    credits: {
+        text: 'getcloudcherry.com',
+        href: 'https://www.getcloudcherry.com/'
+    },
+    xAxis: [{
+        categories: keys,
+    }],
+    yAxis: [{ // Primary yAxis
+        labels: {
+        format: '{value}',
+        style: {
+            color: Highcharts.getOptions().colors[1]
+        }
+        },
+        min: 0,
+        max: 5,
         title: {
-          text: ''
+        text: '',
+        }
+    }, { // Secondary yAxis
+        title: {
+        text: '',
         },
-        tooltip: {
-          formatter: function () {
-            return 'The value for <b>' + this.x + '</b> is <b>' + this.y /* + '</b>, in series ' + this.series.name;*/
-          }
+        min: 0,
+        max: 3000,
+        labels: {
+        format: '{value}',
         },
-        plotOptions: {
-          column: {
-            stacking: 'normal'
-          },
-          line: {
-            dataLabels: {
-              enabled: true
-            },
-            enableMouseTracking: false
-          }
-        },
-        credits: {
-          text: 'getcloudcherry.com',
-          href: 'https://www.getcloudcherry.com/'
-        },
-        xAxis: [{
-          categories: ['Jan 2017', 'Feb 2017', 'Mar 2017', 'May 2017',
-            'Apr 2017',
-            'Jun 2017', 'Jul 2017', 'Aug 2017', 'Sep 2017', 'Oct 2017', 'Nov 2017', 'Dec 2017', 'Jan 2018',
-            'Feb 2018', 'Mar 2018', 'Apr 2018', 'May 2018', 'June 2018'
-          ],
-        }],
-        yAxis: [{ // Primary yAxis
-          labels: {
-            format: '{value}',
-            style: {
-              color: Highcharts.getOptions().colors[1]
-            }
-          },
-          min: 0,
-          max: 5,
-          title: {
-            text: '',
-          }
-        }, { // Secondary yAxis
-          title: {
-            text: '',
-          },
-          min: 0,
-          max: 3000,
-          labels: {
-            format: '{value}',
-          },
-          opposite: true
-        }],
-        legend: {
-          enabled: true
-        },
-        credits: {
-          text: 'getcloudcherry.com',
-          href: 'https://www.getcloudcherry.com/'
-        },
-    
-        series: [{
-            name: 'Cumulative average',
-            type: 'line',
-    
-            data: [3.721, 3.775, 3.806, 3.829, 3.837],
-            color: '#888888',
-            zIndex: 2
-    
-    
-          }, {
-            name: '5',
-            type: 'column',
-            yAxis: 1,
-            data: [1053, 1594, 1146, 977, 341
-            ],
-            color: '#0E9D58'
-    
-          }, {
-            name: '4',
-            type: 'column',
-            yAxis: 1,
-            data: [323, 498, 279, 244, 356],
-            color: '#BFD047'
-    
-          },
-          {
-            name: '3',
-            type: 'column',
-            yAxis: 1,
-            data: [241, 252, 153, 153,153],
-            color: '#FFC105'
-    
-          }, {
-            name: '2',
-            type: 'column',
-            yAxis: 1,
-            data: [152, 109, 106, 82,82],
-            color: '#EF7E14'
-    
-          }, {
-            name: '1',
-            type: 'column',
-            yAxis: 1,
-            data: [462, 294, 295, 274, 955],
-            color: '#D36259'
-    
-          }
-        ]
-      });
-  }
+        opposite: true
+    }],
+    legend: {
+        enabled: true
+    },
+    credits: {
+        text: 'getcloudcherry.com',
+        href: 'https://www.getcloudcherry.com/'
+    },
 
-var toDate, fromDate;
+    series: [{
+        name: 'Cumulative average',
+        type: 'line',
+        //Average Line
+        data: charts.app.avg,
+        color: '#888888',
+        zIndex: 2
+
+
+        }, {
+        name: '5',
+        type: 'column',
+        yAxis: 1,
+        //Green
+        data: Object.values(charts.app.col1),
+        color: '#0E9D58'
+
+        }, {
+        name: '4',
+        type: 'column',
+        yAxis: 1,
+        //Light green
+        data: Object.values(charts.app.col2),
+        color: '#BFD047'
+
+        },
+        {
+        name: '3',
+        type: 'column',
+        yAxis: 1,
+        //Yellow
+        data: Object.values(charts.app.col3),
+        color: '#FFC105'
+
+        }, {
+        name: '2',
+        type: 'column',
+        yAxis: 1,
+        //Orange
+        data: Object.values(charts.app.col4),
+        color: '#EF7E14'
+
+        }, {
+        name: '1',
+        type: 'column',
+        yAxis: 1,
+        //Red 
+        data: Object.values(charts.app.col5),
+        color: '#D36259'
+
+        }
+    ]
+    });
+}
+
+//To get from date
 function getFromDate(){
     fromDate = $("#fromdatepicker").val();
 }
+
+//To save as PDF
 function myFunction() {
   window.print();
 }
 
+//To get to date
 function getToDate(){
     toDate = $("#todatepicker").val();
-    //document.querySelector(".chart").style.diplay = "block";
+    setRating();
+    createTableApp();
+    createTableGplay();
     chart1();
     chart2();
 }
